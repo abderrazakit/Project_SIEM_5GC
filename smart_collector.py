201d07efc3bd08a91f0bf76d7f15b864aa2ed5fb
Abderrazak It <abderrazak.hajji.23ump.ac.ma>
2025-11-25 12:07:45 +0100

    new ip

--- smart_collector.py
+++ smart_collector.py
@@ -11,7 +11,7 @@ from datetime import datetime
 # L'adresse du backend Spring Boot d'Abdellah
 
 
-BACKEND_URL = "http://192.168.1.224:8080/api/logs"
+BACKEND_URL = "http://10.61.234.163:8080/api/logs"
 
 
 # Liste des conteneurs à surveiller (Network Functions)

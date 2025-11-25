f9c50c9c30c27e4be41c67cfdf63edf140db8ee8
aazdag <abdellah.aazdag.23@ump.ac.ma>
2025-11-25 11:41:58 +0100

    add http

--- smart_collector.py
+++ smart_collector.py
@@ -11,7 +11,7 @@ from datetime import datetime
 # L'adresse du backend Spring Boot d'Abdellah
 
 
-BACKEND_URL = "10.61.234.131:8080/api/logs"
+BACKEND_URL = "http://10.61.234.131:8080/api/logs"
 
 
 # Liste des conteneurs à surveiller (Network Functions)
